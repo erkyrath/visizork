3d0408283c3b2f092520f5c279cce21d67e57450
Andrew Plotkin <zarf@ZarfLent.local>
2024-11-27 23:52:09 -0500

    Initial commit, I guess.

---
+++ zillex.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os, os.path
+
+class Lexer:
+    def __init__(self, pathname):
+        self.pathname = pathname
+        _, self.filename = os.path.split(pathname)
+        self.linenum = 0
+        self.charnum = 0
+        self.infl = None
+        self.curchar = None
+
+    def nextchar(self):
+        ch = self.infl.read(1)
+        if not ch:
+            self.curchar = ''
+            return
+        if ch == '\n':
+            self.linenum += 1
+            self.charnum = 0
+        else:
+            self.charnum += 1
+        self.curchar = ch
+
+    def getpos(self):
+        return (self.filename, self.linenum, self.charnum)
+
+    def readtoken(self):
+        while True:
+            ch = self.curchar
+            if not ch:
+                return None
+            if ch in (' ', '\t', '\n'):
+                self.nextchar()
+                continue
+            pos = self.getpos()
+            if ch.isalpha():
+                val = ch
+                self.nextchar()
+                while self.curchar.isalpha() or self.curchar.isdigit() or self.curchar in '-?':
+                    val += self.curchar
+                    self.nextchar()
+                return val, pos
+            if ch.isdigit():
+                val = ch
+                self.nextchar()
+                while self.curchar.isdigit():
+                    val += self.curchar
+                    self.nextchar()
+                val = int(val)
+                return val, pos
+            if ch == '-':
+                val = ''
+                self.nextchar()
+                if self.curchar.isdigit():
+                    while self.curchar.isdigit():
+                        val += self.curchar
+                        self.nextchar()
+                    val = -int(val)
+                    return val, pos
+                else:
+                    return '-', pos
+            if ch == '"':
+                val = ''
+                self.nextchar()
+                while self.curchar and self.curchar != '"':
+                    if self.curchar == '|':
+                        self.nextchar()
+                        if self.curchar != '\n':
+                            raise Exception('| not followed by newline')
+                        val += '\n'
+                    elif self.curchar == '\\':
+                        self.nextchar()
+                        if self.curchar != '"':
+                            raise Exception('\\ not followed by "')
+                        val += '"'
+                    elif self.curchar == '\n':
+                        val += ' '
+                    else:
+                        val += self.curchar
+                    self.nextchar()
+                if not self.curchar:
+                    raise Exception('unterminated string')
+                self.nextchar()
+                return val, pos
+            self.nextchar()
+            return ch, pos
+
+    def readfile(self):
+        self.infl = open(self.pathname)
+        self.nextchar()
+        while True:
+            tok = self.readtoken()
+            if tok is None:
+                break
+            print(tok)
+        self.infl.close()
+        self.infl = None
+            
+
+lex = Lexer('test.zil')
+lex.readfile()
