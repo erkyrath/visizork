d4b53d8669827afd6457c89d971d19578ee3ce95
Andrew Plotkin <zarf@ZarfLent.local>
2024-11-28 21:30:20 -0500

    Parsing the ifdef %COND case.

--- zilana.py
+++ zilana.py
@@ -7,6 +7,7 @@ class Zcode:
 
     def build(self):
         self.findcomments(self.tokls)
+        self.findifdefs(self.tokls)
         self.findglobals()
 
     def findcomments(self, ls, incomment=False):
@@ -20,6 +21,24 @@ class Zcode:
             if tok.typ is TokType.GROUP:
                 self.findcomments(tok.children, incomment=subcomment)
                 
+    def findifdefs(self, ls, inifdef=False):
+        for tok in ls:
+            subifdef = inifdef
+            if tok.typ is TokType.GROUP and tok.val == '%' and tok.children:
+                ctok = tok.children[0]
+                if ctok.matchform('COND', 0):
+                    for cgrp in ctok.children[ 1 : ]:
+                        keep = True
+                        match = iseqzorknum(cgrp)
+                        if match:
+                            print('### zorknum', match, cgrp)
+                            keep = (match == 1)
+                        ###
+            if inifdef:
+                tok.ifdef = True
+            if tok.typ is TokType.GROUP:
+                self.findifdefs(tok.children, inifdef=subifdef)
+                
     def findglobals(self):
         for tok in self.tokls:
             if tok.comment:
@@ -32,3 +51,16 @@ class Zcode:
                     self.globals.append( (idtok.val, tok.pos) )
                     
                                      
+def iseqzorknum(cgrp):
+    if cgrp.typ is TokType.GROUP and cgrp.val == '()' and len(cgrp.children) == 2:
+        condgrp = cgrp.children[0]
+        resgrp = cgrp.children[1]
+        if condgrp.matchform('==?', 2):
+            keytok = condgrp.children[1]
+            valtok = condgrp.children[2]
+            if keytok.typ is TokType.GROUP and keytok.val == "," and keytok.children:
+                kidtok = keytok.children[0]
+                if kidtok.typ is TokType.ID and kidtok.val == 'ZORK-NUMBER':
+                    if valtok.typ is TokType.NUM:
+                        return valtok.num
+                    
