c217f9061e5d3fe6dc55c5821bfc203f21ee5c71
Andrew Plotkin <zarf@ZarfLent.local>
2024-11-29 20:49:18 -0500

    Redo the ifdef stripper in a sensible way.

--- parse.py
+++ parse.py
@@ -2,12 +2,14 @@
 
 import sys
 from zillex import Lexer, dumptokens
-from zilana import Zcode, markcomments, stripcomments
+from zilana import Zcode
+from zilana import markcomments, stripcomments
+from zilana import stripifdefs
 
 for filename in sys.argv[1:]:
     lex = Lexer(filename)
     ls = lex.readfile(includes=True)
-    stripcomments(ls)
+    stripifdefs(ls)
     dumptokens(ls, withpos=False, skipdead=False)
     #zcode = Zcode(ls)
     #zcode.build()
--- zilana.py
+++ zilana.py
@@ -23,6 +23,41 @@ def stripcomments(ls):
     ls.clear()
     ls.extend(newls)
 
+def stripifdefs(ls):
+    newls = []
+    for tok in ls:
+        if tok.typ is TokType.GROUP and tok.val == '%' and tok.children:
+            ctok = tok.children[0]
+            if ctok.matchform('COND', 0):
+                found = None
+                for cgrp in ctok.children[ 1 : ]:
+                    match = iseqzorknum(cgrp)
+                    if match:
+                        if match[0] == 1:
+                            found = match[1]
+                            break
+                if found:
+                    newls.append(found)
+                continue
+        newls.append(tok)
+        if tok.typ is TokType.GROUP:
+            stripifdefs(tok.children)
+    ls.clear()
+    ls.extend(newls)
+
+def iseqzorknum(cgrp):
+    if cgrp.typ is TokType.GROUP and cgrp.val == '()' and len(cgrp.children) == 2:
+        condgrp = cgrp.children[0]
+        resgrp = cgrp.children[1]
+        if condgrp.matchform('==?', 2):
+            keytok = condgrp.children[1]
+            valtok = condgrp.children[2]
+            if keytok.typ is TokType.GROUP and keytok.val == "," and keytok.children:
+                kidtok = keytok.children[0]
+                if kidtok.typ is TokType.ID and kidtok.val == 'ZORK-NUMBER':
+                    if valtok.typ is TokType.NUM:
+                        return (valtok.num, resgrp)
+                    
 
 class Zcode:
     def __init__(self, tokls):
@@ -33,24 +68,6 @@ class Zcode:
         #self.findifdefs(self.tokls)
         self.findglobals()
 
-    def findifdefs(self, ls, inifdef=False):
-        for tok in ls:
-            subifdef = inifdef
-            if tok.typ is TokType.GROUP and tok.val == '%' and tok.children:
-                ctok = tok.children[0]
-                if ctok.matchform('COND', 0):
-                    for cgrp in ctok.children[ 1 : ]:
-                        keep = True
-                        match = iseqzorknum(cgrp)
-                        if match:
-                            print('### zorknum', match, cgrp)
-                            keep = (match == 1)
-                        ###
-            if inifdef:
-                tok.ifdef = True
-            if tok.typ is TokType.GROUP:
-                self.findifdefs(tok.children, inifdef=subifdef)
-                
     def findglobals(self):
         for tok in self.tokls:
             if tok.comment:
@@ -63,16 +80,3 @@ class Zcode:
                     self.globals.append( (idtok.val, tok.pos) )
                     
                                      
-def iseqzorknum(cgrp):
-    if cgrp.typ is TokType.GROUP and cgrp.val == '()' and len(cgrp.children) == 2:
-        condgrp = cgrp.children[0]
-        resgrp = cgrp.children[1]
-        if condgrp.matchform('==?', 2):
-            keytok = condgrp.children[1]
-            valtok = condgrp.children[2]
-            if keytok.typ is TokType.GROUP and keytok.val == "," and keytok.children:
-                kidtok = keytok.children[0]
-                if kidtok.typ is TokType.ID and kidtok.val == 'ZORK-NUMBER':
-                    if valtok.typ is TokType.NUM:
-                        return valtok.num
-                    
