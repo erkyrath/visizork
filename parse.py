789d4e0d95fa4b3b096f7d4d531932cef0bf1667
Andrew Plotkin <zarf@ZarfLent.local>
2024-11-30 22:14:31 -0500

    Some options.

--- parse.py
+++ parse.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
+import optparse
+
 from zillex import Lexer, dumptokens
 from zilana import Zcode
 from zilana import markcomments, stripcomments
 from zilana import stripifdefs
 from txdparse import TXDData
 
-'''
-for filename in sys.argv[1:]:
-    lex = Lexer(filename)
+popt = optparse.OptionParser()
+
+popt.add_option('-z', '--zil',
+                action='store', dest='zilfile')
+popt.add_option('-t', '--txd',
+                action='store_true', dest='txdfile')
+
+(opts, args) = popt.parse_args()
+
+if opts.zilfile:
+    lex = Lexer(opts.zilfile)
     ls = lex.readfile(includes=True)
     stripcomments(ls)
     stripifdefs(ls)
-    #dumptokens(ls, withpos=False, skipdead=False)
     zcode = Zcode(ls)
     zcode.build()
-    for val in reversed(zcode.objects):
-        print(val)
-'''
+    print('globals:', len(zcode.globals))
+    print('routines:', len(zcode.routines))
+    print('objects:', len(zcode.objects))
 
-dat = TXDData()
-dat.readdump('gamedat/game-dump.txt')
-print('routines:', len(dat.routines))
-print('strings:', len(dat.strings))
-print('istrings:', len(dat.istrings))
+if opts.txdfile:
+    print('reading TXD dump...')
+    dat = TXDData()
+    dat.readdump('gamedat/game-dump.txt')
+    print('routines:', len(dat.routines))
+    print('strings:', len(dat.strings))
+    print('istrings:', len(dat.istrings))
