6be4c55a97c6edcf0e7b0aa326cb953a3ec5d47b
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-13 20:14:01 -0500

    Clean up gamedat.js a bit.

--- src/game/gamedat.js
+++ src/game/gamedat.js
@@ -62,21 +62,9 @@ window.gamedat_ids = {};
         gamedat_object_names.set(obj.name, obj);
         if (obj.isroom)
             gamedat_object_room_ids.add(obj.onum);
-        //### unhardcode
-        /* The global container itself counts as a global for our purposes. */
-        if (obj.origparent == 247 || obj.onum == 247)
-            gamedat_object_global_ids.add(obj.onum);
-        
-        if (obj.isroom)
-            gamedat_object_treesort.set(obj.onum, 1);
-        else if (obj.name == 'PSEUDO-OBJECT')
-            gamedat_object_treesort.set(obj.onum, 3);
-        else if (gamedat_object_global_ids.has(obj.onum))
-            gamedat_object_treesort.set(obj.onum, 4);
-        else
-            gamedat_object_treesort.set(obj.onum, 2);
-    }
 
+    }
+    
     for (let tup of window.gamedat_strings) {
         gamedat_string_map.set(tup[0], { text:tup[1], sourceloc:tup[2] });
     }
@@ -101,5 +89,20 @@ window.gamedat_ids = {};
     gamedat_ids.STARTROOM = gamedat_object_names.get('WEST-OF-HOUSE').onum;
     gamedat_ids.PSEUDO_OBJECT = gamedat_object_names.get('PSEUDO-OBJECT').onum;
 
+    for (let obj of window.gamedat_objects) {
+        /* The global container itself counts as a global for our purposes. */
+        if (obj.origparent == gamedat_ids.GLOBAL_OBJECTS || obj.onum == gamedat_ids.GLOBAL_OBJECTS)
+            gamedat_object_global_ids.add(obj.onum);
+        
+        if (obj.isroom)
+            gamedat_object_treesort.set(obj.onum, 1);
+        else if (obj.name == 'PSEUDO-OBJECT')
+            gamedat_object_treesort.set(obj.onum, 3);
+        else if (gamedat_object_global_ids.has(obj.onum))
+            gamedat_object_treesort.set(obj.onum, 4);
+        else
+            gamedat_object_treesort.set(obj.onum, 2);
+    }
+    
 })();
 
