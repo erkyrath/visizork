9501747ca749518056e7d2aef29f9166c1e8abb1
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-14 11:11:49 -0500

    Release time.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
-const datestr = 'Jan 13, 2025'
-const release = false;
+const datestr = 'Jan 14, 2025'
+const release = true;
 
 const nodeenv = (release ? 'production' : 'development')
 const tersopt = { format: { ascii_only:true } };
