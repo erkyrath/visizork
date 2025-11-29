44b1600b858de096944c408b07b1491adb419bc9
Andrew Plotkin <zarf@ZarfLent.local>
2025-11-28 23:41:20 -0500

    Working on style.

--- src/visi/map.tsx
+++ src/visi/map.tsx
@@ -4,7 +4,7 @@ export function GameMap()
 {
     return (
         <div className="ScrollXYContent">
-            <img width="1200" height="800" src="css/zorkmap.svg" />
+            <img className="GameMap" width="1200" height="800" src="css/zorkmap.svg" />
         </div>
     );
 }
