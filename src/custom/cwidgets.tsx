7d52e21bd752e351f8594d2c232acf23ffb52ac6
Andrew Plotkin <zarf@ZarfLent.local>
2025-12-23 00:10:37 -0500

    Got the sorter wired up.

--- src/custom/cwidgets.tsx
+++ src/custom/cwidgets.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-export function ObjListSorter()
+import { ZObject } from '../visi/zstate';
+import { gamedat_ids, gamedat_distances, gamedat_object_treesort } from './gamedat';
+
+export function sorter_for_key(key: number) : (roots:ZObject[], map:Map<number, ZObject>) => void
 {
-    const [ follow, setFollow ] = useState('adv');
+    let originobj: number = (key ? key : gamedat_ids.ADVENTURER);
+
+    return function(roots: ZObject[], map: Map<number, ZObject>) {
+        let advroom = originobj;
+
+        while (true) {
+            let tup = map.get(advroom);
+            if (!tup || tup.parent == 0 || tup.parent == gamedat_ids.ROOMS)
+                break;
+            advroom = tup.parent;
+        }
+        
+        if (!gamedat_distances[advroom])
+            advroom = gamedat_ids.STARTROOM;
+        let distmap = gamedat_distances[advroom];
+
+        roots.sort((o1, o2) => {
+            let sort1 = gamedat_object_treesort.get(o1.onum) ?? 0;
+            let sort2 = gamedat_object_treesort.get(o2.onum) ?? 0;
+            if (sort1 != sort2)
+                return sort1 - sort2;
+            if (sort1 == 1)
+                return distmap[o1.onum] - distmap[o2.onum];
+            return (o1.onum - o2.onum);
+        });
+    }
+}
+
+export function ObjListSorter({ followKey, setFollowKey } : { followKey:number, setFollowKey:(v:number)=>void })
+{
+    let follow: string = (followKey == 0) ? 'adv' : 'thief';
     
     function evhan_follow_change(val: string) {
-        setFollow(val);
+        if (val == 'adv')
+            setFollowKey(0);
+        else if (val == 'thief')
+            setFollowKey(gamedat_ids.THIEF);
     }
     
     return (
