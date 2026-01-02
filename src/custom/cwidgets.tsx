add5c62a4cdc917e9536642b0286da3e396f3922
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-01 21:58:12 -0500

    PERFORMO/PERFORMI.

--- src/custom/cwidgets.tsx
+++ src/custom/cwidgets.tsx
@@ -4,6 +4,8 @@ import { useState, useContext, createContext } from 'react';
 import { ZObject } from '../visi/zstate';
 import { ObjectData, GlobalData } from './gamedat';
 import { gamedat_ids, gamedat_distances, gamedat_object_treesort } from './gamedat';
+import { ArgShowObject, ArgShowProperty } from '../visi/actshowers';
+import { StackCallCtx } from '../visi/context';
 
 export function contains_label(obj: ObjectData) : string
 {
@@ -72,3 +74,28 @@ export function global_value_display(tag: string, value: number, glo: GlobalData
 {
     return null;
 }
+
+export function stack_call_arg_display(tag: string, value: number) : JSX.Element|null
+{
+    switch (tag) {
+        
+    case 'PERFORMO':
+        let ctx = useContext(StackCallCtx);
+        if (ctx.args[0] == 137) {      /* verb WALK */
+            return (
+                <ArgShowProperty value={ value } />
+            );
+        }
+        return (
+            <ArgShowObject value={ value } />
+        )
+        
+    case 'PERFORMI':
+        return (
+            <ArgShowObject value={ value } />
+        )
+    }
+
+    return null;
+}
+
