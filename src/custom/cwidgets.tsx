98167e14cc68ff036b0cb9ad602d3752a407626a
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-21 21:46:40 -0500

    Comment.

--- src/custom/cwidgets.tsx
+++ src/custom/cwidgets.tsx
@@ -81,7 +81,7 @@ export function stack_call_arg_display(tag: string, value: number) : JSX.Element
         
     case 'PERFORMO':
         let ctx = useContext(StackCallCtx);
-        if (ctx.args[0] == 137) {      /* verb WALK */
+        if (ctx.args[0] == 137) {      /* action WALK */
             return (
                 <ArgShowProperty value={ value } />
             );
